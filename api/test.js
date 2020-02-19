e7f994d98fb961ac52176758b608acdccf6c2aeb
Áron Kárpáti <aronkarpati@Arons-MacBook-Pro.local>
2020-02-19 12:44:58 +0100

    meeting

--- api/test.js
+++ api/test.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
 	res.status(200).json({ "ML": "it works" });
 });
 
+router.get('/meeting', (req, res) => {
+	res.status(200).json({ "ML": "im on the meeting" });
+});
+
+
 module.exports = router
